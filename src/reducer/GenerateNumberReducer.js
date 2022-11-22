fbf07a0151cbb45d7050c34e33ad361975eb6734
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-23 01:18:36 +0300

    Custom reducer created

---
+++ src/reducer/GenerateNumberReducer.js
@@ -0,0 +1,69 @@
+export const generateNumberReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_MIN": {
+      return {
+        ...state,
+        min: parseInt(action.value),
+      };
+    }
+    case "SET_MAX": {
+      return {
+        ...state,
+        max: parseInt(action.value),
+      };
+    }
+    case "SET_TEMP_MIN": {
+      return {
+        ...state,
+        tempMin: action.value,
+      };
+    }
+    case "SET_TEMP_MAX": {
+      return {
+        ...state,
+        tempMax: action.value,
+      };
+    }
+    case "SET_COUNT": {
+      return {
+        ...state,
+        count: action.value,
+      };
+    }
+    case "TOGGLE_LESS_SIGN": {
+      return {
+        ...state,
+        lessSign: "<" == state.lessSign ? "<=" : "<",
+      };
+    }
+    case "SET_UNIQUE_NUMBERS": {
+      return {
+        ...state,
+        uniqueNumbers: action.value,
+      };
+    }
+    case "TOGGLE_GREATER_SIGN": {
+      return {
+        ...state,
+        greaterSign: "<" == state.greaterSign ? "<=" : "<",
+      };
+    }
+    case "CHECK_CONSTRAINTS": {
+      if (parseInt(state.tempMax) < parseInt(state.tempMin)) {
+        return {
+          ...state,
+          min: state.tempMax,
+          max: state.tempMin,
+          tempMin: state.tempMax,
+          tempMax: state.tempMin,
+        };
+      } else {
+        return {
+          ...state,
+          max: state.tempMax,
+          min: state.tempMin,
+        };
+      }
+    }
+  }
+};
