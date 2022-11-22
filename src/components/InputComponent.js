075ed88d0eacebddb6d10d3add6510036b3e4eb0
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-23 01:17:13 +0300

    InputComponent created

---
+++ src/components/InputComponent.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+
+export const InputComponent = (props) => {
+  const { onChangeNumber, number, placeholder, onEndEditing, isApproved } =
+    props;
+  return (
+    <SafeAreaView style={styles.root}>
+      <TextInput
+        style={[
+          styles.input,
+          !isApproved
+            ? { color: "red", borderColor: "red" }
+            : { color: "black", borderColor: "black" },
+        ]}
+        onEndEditing={onEndEditing}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder={placeholder}
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 45,
+    margin: 5,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
